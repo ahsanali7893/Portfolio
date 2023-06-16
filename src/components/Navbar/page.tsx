c512b8a2b351c180b8d7910260657fca0b53ad8b
ahsanali7893 <ahsan3916902@gmail.com>
2023-06-16 17:17:39 +0500

    initial changes

--- src/components/Navbar/page.tsx
+++ src/components/Navbar/page.tsx
@@ -1,30 +1,32 @@
-import React from 'react'
-import Link from 'next/link'
-
+import React from 'react';
+import Link from 'next/link';
 
 const Navbar = () => {
   return (
     <nav className='fixed w-full h-24 shadow-xl' style={{ backgroundColor: 'rgba(0, 128, 128, 0.6)' }}>
       <div className='flex justify-between items-center h-full w-full px-4 2xl:px-16'>
-      <Link href={'/'}><h1 className='cursor-pointer font-extrabold'>AHSAN.</h1></Link>
-      <div>
-        <ul className='hidden sm:flex'>
-          <Link href={'/about'}>
-            <li className='ml-10 uppercase hover:border-b text-xl'>About</li>
-          </Link>
-          <Link href={'/service'}>
-            <li className='ml-10 uppercase hover:border-b text-xl'>Service</li>
-          </Link>
-          <Link href={'/portfolio'}>
-            <li className='ml-10 uppercase hover:border-b text-xl'>Portfolio</li>
-          </Link>
-          <Link href={'/contact'}>
-            <li className='ml-10 uppercase hover:border-b text-xl'>Contact</li>
-          </Link>
-        </ul>
-      </div>
+        <Link href='/'>
+          <h1 className='text-xl font-extrabold'>AHSAN.</h1>
+        </Link>
+        <div>
+          <ul className='hidden sm:flex'>
+            <li className='ml-10 font-bold uppercase hover:border-b text-sm cursor-pointer'>
+              <Link href='/about'>About</Link>
+            </li>
+            <li className='ml-10 font-bold uppercase hover:border-b text-sm cursor-pointer'>
+              <Link href='/service'>Service</Link>
+            </li>
+            <li className='ml-10 font-bold uppercase hover:border-b text-sm cursor-pointer'>
+              <Link href='/portfolio'>Portfolio</Link>
+            </li>
+            <li className='ml-10 font-bold uppercase hover:border-b text-sm cursor-pointer'>
+              <Link href='/contact'>Contact</Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
-  )
-}
-export default Navbar
+  );
+};
+
+export default Navbar;
