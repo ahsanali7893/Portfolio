b0a1cb018f44db07734097450bafc19ad0612a6d
ahsanali7893 <ahsan3916902@gmail.com>
2023-06-16 16:01:04 +0500

    Initial changes

--- src/components/Navbar/page.tsx
+++ src/components/Navbar/page.tsx
@@ -4,7 +4,12 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <nav className='fixed w-full h-24 shadow-xl'></nav>
+    <nav className='fixed w-full h-24 shadow-xl' style={{ backgroundColor: 'rgba(0, 128, 128, 0.6)' }}>
+      <div className='flex justify-between items-center h-full w-full px-4 2xl:px-16'>
+      <h1 className='cursor-pointer font-extrabold'>AHSAN.</h1>
+      <div>Right Side</div>
+      </div>
+    </nav>
   )
 }
 export default Navbar
