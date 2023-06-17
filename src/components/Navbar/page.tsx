a7daac991ccd70f643c65f39d7c7da366407bd5e
ahsanali7893 <ahsan3916902@gmail.com>
2023-06-17 14:17:42 +0500

    initial change

--- src/components/Navbar/page.tsx
+++ src/components/Navbar/page.tsx
@@ -17,7 +17,7 @@ const Navbar = () => {
   };
 
   return (
-    <div className="w-full z-50 top-0 py-3 sm:py-5 shadow-xl sticky" style={{ backgroundColor: 'rgba(0, 128, 128, 0.6)' }}>
+    <div className="w-full opacity-1000 z-50 top-0 py-3 sm:py-5 shadow-xl sticky" style={{ backgroundColor: 'rgba(0, 128, 128, 0.6)' }}>
       <div className="container flex items-center h-full w-full 2xl:px-16 justify-between px-5">
         <div>
           <Link href="/">
