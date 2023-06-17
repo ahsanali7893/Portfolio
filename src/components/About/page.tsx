4921873bac46bbd1aff4a97e2a55b76ad91330f4
ahsanali7893 <ahsan3916902@gmail.com>
2023-06-17 18:48:14 +0500

    Initial changes

--- src/components/About/page.tsx
+++ src/components/About/page.tsx
@@ -66,10 +66,45 @@ const About = () => {
           Skills
         </h2>
         <div className="grid grid-cols-1 gap-6 pt-10 sm:grid-cols-2 md:gap-10 md:pt-12 lg:grid-cols-3">
-          <div className="group rounded px-8 py-12 shadow hover:bg-primary">
+          <div className="group rounded-full px-8 py-12">
             <div className="mx-auto h-24 w-24 text-center xl:h-28 xl:w-28">
-              <div className="hover:">
+              <div className="hover:translate-x-2 duration-500">
                 <img src="./html.png" alt="html" className="cursor-pointer " />
+              </div>
+            </div>
+          </div>
+          <div className="group rounded  px-8 py-12">
+            <div className="mx-auto h-24 w-24 text-center xl:h-28 xl:w-28">
+              <div className="hover:translate-x-[-8px] duration-500">
+                <img src="./css.png" alt="html" className="cursor-pointer " />
+              </div>
+            </div>
+          </div>
+          <div className="group rounded px-8 py-12">
+            <div className="mx-auto h-24 w-24 text-center xl:h-28 xl:w-28">
+              <div className="hover:translate-x-[-8px] duration-500">
+                <img src="./tailwind.png" alt="html" className="cursor-pointer " />
+              </div>
+            </div>
+          </div>
+          <div className="group rounded px-8 py-12">
+            <div className="mx-auto h-24 w-24 text-center xl:h-28 xl:w-28">
+              <div className="hover:translate-x-2 duration-500">
+                <img src="./js.png" alt="html" className="cursor-pointer " />
+              </div>
+            </div>
+          </div>
+          <div className="group rounded px-8 py-12">
+            <div className="mx-auto h-24 w-24 text-center xl:h-28 xl:w-28">
+              <div className="hover:translate-x-[-8px] duration-500">
+                <img src="./react.png" alt="html" className="cursor-pointer " />
+              </div>
+            </div>
+          </div>
+          <div className="group rounded px-8 py-12">
+            <div className="mx-auto  h-24 w-24 text-center xl:h-28 xl:w-28">
+              <div className="hover:translate-x-[-8px] duration-500">
+                <img src="./next.png" alt="html" className="cursor-pointer" />
               </div>
             </div>
           </div>
