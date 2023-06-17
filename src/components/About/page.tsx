ffc0c12e958d1a4b9c12b1e91c5d43b808e5a76f
ahsanali7893 <ahsan3916902@gmail.com>
2023-06-17 17:35:09 +0500

    Initial changes

--- src/components/About/page.tsx
+++ src/components/About/page.tsx
@@ -61,20 +61,20 @@ const About = () => {
           </div>
         </div>
       </div>
-        <div className="container py-16 md:py-20">
-          <h2 className="text-center font-header text-4xl font-semibold uppercase text-[#66b3b3] text-primary sm:text-5xl lg:text-6xl">
-            Skills
-          </h2>
-          <div className="grid grid-cols-1 gap-6 pt-10 sm:grid-cols-2 md:gap-10 md:pt-12 lg:grid-cols-3">
-            <div className="group rounded px-8 py-12 shadow hover:bg-primary">
-                <div className="mx-auto h-24 w-24 text-center xl:h-28 xl:w-28">
-                    <div className="hover:">
-                        <img src="./html.png" alt="html" className="cursor-pointer "  />
-                    </div>
-                </div>
+      <div className="container py-16 md:py-20">
+        <h2 className="text-center font-header text-4xl font-semibold uppercase text-[#66b3b3] text-primary sm:text-5xl lg:text-6xl">
+          Skills
+        </h2>
+        <div className="grid grid-cols-1 gap-6 pt-10 sm:grid-cols-2 md:gap-10 md:pt-12 lg:grid-cols-3">
+          <div className="group rounded px-8 py-12 shadow hover:bg-primary">
+            <div className="mx-auto h-24 w-24 text-center xl:h-28 xl:w-28">
+              <div className="hover:">
+                <img src="./html.png" alt="html" className="cursor-pointer " />
+              </div>
             </div>
           </div>
         </div>
+      </div>
     </div>
   );
 };
