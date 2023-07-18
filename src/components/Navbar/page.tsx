662cba820271f4445520d3c1fefdf6777e52625d
ahsanali7893 <ahsan3916902@gmail.com>
2023-07-18 12:27:06 +0500

    some changes

--- src/components/Navbar/page.tsx
+++ src/components/Navbar/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 import React, { useState } from "react";
 import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 import Link from "next/link";
@@ -12,7 +12,7 @@ const Navbar = () => {
 
   return (
     <div
-      className="w-full opacity-1000 z-50 top-0 py-3 sm:py-5 shadow-xl sticky "
+      className="w-full  opacity-1000 z-50 top-0 py-3 sm:py-5 shadow-xl sticky "
       style={{ backgroundColor: "rgba(0, 128, 128, 0.6)" }}
     >
       <div className="container flex items-center h-full w-full 2xl:px-16 justify-between px-5 max-w-6xl mx-auto">
@@ -23,8 +23,8 @@ const Navbar = () => {
             </h1>
           </Link>
         </div>
-        <div className=" hidden sm:flex">
-          <div className="flex items-center text-sm">
+        <div className="hidden sm:flex">
+          <div className="flex items-center py-7 text-sm">
             <li className="group pl-6 list-none py-4">
               <Link
                 href={"#about"}
@@ -80,7 +80,7 @@ const Navbar = () => {
           </div>
         </div>
         <div className="flex-col py-9 sm:py-10 text-center">
-          <li className="group pl-6 list-none">
+          <li className="group pl-6 list-none py-4">
             <Link
               href={"#about"}
               className="cursor-pointer pt-0.5 font-header font-semibold uppercase text-white"
@@ -89,8 +89,8 @@ const Navbar = () => {
             </Link>
             <span className="block h-0.5 w-full bg-transparent group-hover:bg-yellow-500"></span>
           </li>
-          <li className="group pl-6 list-none">
-            <Link  
+          <li className="group pl-6 list-none py-4">
+            <Link
               href={"#skill"}
               className="cursor-pointer pt-0.5 font-header font-semibold uppercase text-white"
             >
@@ -98,8 +98,7 @@ const Navbar = () => {
             </Link>
             <span className="block h-0.5 w-full bg-transparent group-hover:bg-yellow-500"></span>
           </li>
-
-          <li className="group pl-6 list-none">
+          <li className="group pl-6 list-none py-4">
             <Link
               href={"#portfolio"}
               className="cursor-pointer pt-0.5 font-header font-semibold uppercase text-white"
@@ -108,7 +107,7 @@ const Navbar = () => {
             </Link>
             <span className="block h-0.5 w-full bg-transparent group-hover:bg-yellow-500"></span>
           </li>
-          <li className="group pl-6 list-none">
+          <li className="group pl-6 list-none py-4">
             <Link
               href={"#contact"}
               className="cursor-pointer pt-0.5 font-header font-semibold uppercase text-white"
