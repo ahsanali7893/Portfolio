0fb553d3be3d06b531d037c13c1cca48fc579e52
ahsanali7893 <ahsan3916902@gmail.com>
2023-07-27 16:55:52 +0500

    initial changes

--- src/components/Contact/page.tsx
+++ src/components/Contact/page.tsx
@@ -1,84 +1,278 @@
-import { AiOutlineMail } from "react-icons/ai";
-import { BsFillTelephoneFill } from "react-icons/bs";
-import { BiCurrentLocation } from "react-icons/bi";
+"use client";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
+import { BsWhatsapp } from "react-icons/bs";
+import { GoMail } from "react-icons/go";
+import { HiOutlineLocationMarker } from "react-icons/hi";
+import { ToastContainer, toast } from "react-toastify";
+import { RxCrossCircled } from "react-icons/rx";
+import "react-toastify/dist/ReactToastify.css";
 
-type Props = {};
+export default function Contact() {
+  const [loading, setLoading] = useState(false);
+  const formik = useFormik({
+    initialValues: {
+      user_name: "",
+      user_email: "",
+      user_subject: "",
+      user_message: "",
+    },
+    validationSchema: Yup.object().shape({
+      user_name: Yup.string()
+        .min(3, "Name must be more than 3 character")
+        .max(65, "Enter your name with 65 characters or less")
+        .required("Please enter your name"),
+      user_subject: Yup.string()
+        .min(3, "Subject must be more than 3 character")
+        .max(225, "Enter Subject with 225 characters or less")
+        .required("Please enter your subject"),
+      user_email: Yup.string()
+        .email("Invalid email")
+        .max(225, "email address with 225 characters or less")
+        .required("Please enter your email"),
+      user_message: Yup.string()
+        .min(3, "message must be more than 3 character")
+        .max(250, "Enter message with 250 characters or less")
+        .required("Please enter your message"),
+    }),
+    onSubmit: (values: any) => {
+      handleSaveAction(values);
+    },
+    enableReinitialize: true,
+  });
+  const { values, handleChange, handleSubmit, handleBlur, errors, touched } =
+    formik;
 
-const Contact = (props: Props) => {
+  const handleSaveAction = (formValue: any) => {
+    sendEmail(formValue);
+  };
+  // console.log(errors, "errors");
+  const form = useRef<HTMLFormElement>(null);
+
+  const sendEmail = (e: any) => {
+    setLoading(true);
+    emailjs
+      .send("service_2d241zq", "template_3mtzjmk", e, "LQe7qqYP6HhGvunpL")
+      .then(
+        (result) => {
+          // console.log(result.text);
+          const notify = () =>
+            toast("Message sent", {
+              position: "top-center",
+              autoClose: 5000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "light",
+            });
+          values.user_name = "";
+          values.user_email = "";
+          values.user_subject = "";
+          values.user_message = "";
+          notify();
+          setLoading(false);
+        },
+        (error) => {
+          console.log(error.text);
+          const notify = () =>
+            toast("Error while sending message", {
+              position: "top-center",
+              autoClose: 5000,
+              hideProgressBar: false,
+              closeOnClick: true,
+              pauseOnHover: true,
+              draggable: true,
+              progress: undefined,
+              theme: "light",
+            });
+          notify();
+          setLoading(false);
+        }
+      );
+  };
+
   return (
-    <section id="contact">
-      <div className="container py-16 md:py-20 max-w-4xl mx-auto px-5">
-        <h2 className="text-center font-header text-4xl font-semibold uppercase text-[#66b3b3] sm:text-5xl lg:text-6xl">
-          Here&apos;s a contact form
-        </h2>
-        <h4 className="pt-6 text-center font-header text-xl font-medium text-black sm:text-2xl lg:text-3xl">
-          Have&apos; Any Questions?
-        </h4>
-        <div className="mx-auto w-full pt-5 text-center sm:w-2/3 lg:pt-6"></div>
-        <form className="mx-auto w-full pt-10 sm:w-3/4">
-          <div className="flex flex-col md:flex-row">
-            <input
-              className="mr-3 w-full rounded border border-gray-200 px-4 py-3 font-body text-black md:w-1/2 lg:mr-5"
-              type="text"
-              placeholder="Name"
-              id="name"
-            />
-            <input
-              className="mt-6 w-full rounded border border-gray-200 px-4 py-3 font-body md:ml-3 md:mt-0 text-black md:w-1/2 lg:ml-5"
-              type="text"
-              placeholder="Email"
-              id="email"
-            />
+    <>
+      <ToastContainer />
+      <div className="pt-20 max-w-screen-2xl min-h-screen px-5 mt-20 md:px-10 lg:px-32 mx-auto flex items-cente">
+        <div className="w-full ">
+          <div className="text-2xl items-center justify-center text-center">
+            <h2 className="font-bold text-3xl md:text-4xl tracking-widest uppercase text-white">
+              CONTACT <span className="text-[#ffb200]"> ME</span>
+            </h2>
+            {/* <button onClick={notify}>Notify !</button> */}
           </div>
-          <textarea
-            className="mt-6 w-full rounded border border-gray-200 px-4 py-3 font-body text-black md:mt-8"
-            placeholder="Message"
-            id="message"
-            cols={30}
-            rows={10}
-          ></textarea>
-          <button className="mt-6 mx-auto flex items-center justify-center rounded bg-gray-400 px-8 py-3 font-header text-lg font-bold uppercase text-white hover:bg-sky-400 hover:text-white duration-500">
-            Send
-          </button>
-        </form>
-        <div className="flex flex-col pt-16 lg:flex-row">
-          <div className="w-full border-l-2 border-t-2 border-r-2 border-b-2 border-grey-60 px-6 py-6 sm:py-8 lg:w-1/3">
-            <div className="flex items-center">
-              <BsFillTelephoneFill className="text-2xl text-grey-40" />
-              <p className="pl-2 font-body font-bold uppercase text-grey-40 lg:text-lg">
-                My Phone
-              </p>
+          <div className="my-10">
+            <div className="lg:flex pt-6">
+              <div className="w-full lg:w-[40%]">
+                <h1 className="pb-6 font-bold text-3xl text-white tracking-widest">
+                  (: DON&apos;T BE{" "}
+                  <span className="text-[#ffb400]"> SHY !</span>
+                </h1>
+                <p className="text-white">
+                  I&lsquo;m open to discussing new projects, creative ideas, and
+                  opportunities to be part of your vision. Let&lsquo;s connect
+                  and explore possibilities together. Feel free to reach out for
+                  a free consultation using the contact information below.
+                </p>
+                <div className="flex items-center mt-8">
+                  <div className="">
+                    <HiOutlineLocationMarker
+                      className="text-[#ffb400] mr-4"
+                      size={40}
+                    />
+                  </div>
+                  <div className="">
+                    <h2 className=" font-bold text-xl text-white tracking-widest">
+                      Address Point
+                    </h2>
+                    <p className="text-white">G-10, Islamabad, Pakistan .</p>
+                  </div>
+                </div>
+                <div className="flex items-center mt-8">
+                  <div className="">
+                    <GoMail className="text-[#ffb400] mr-4" size={40} />
+                  </div>
+                  <div className="">
+                    <h2 className=" font-bold text-xl text-white tracking-widest">
+                      Mail Me
+                    </h2>
+                    <p className="text-white">contact@khalilahmed.dev</p>
+                  </div>
+                </div>
+                <div className="flex items-center mt-8">
+                  <div className="">
+                    <BsWhatsapp className="text-[#ffb400] mr-4" size={35} />
+                  </div>
+                  <div className="">
+                    <h2 className=" font-bold text-xl text-white tracking-widest">
+                      Whatsapp
+                    </h2>
+                    <p className="text-white">+923070599766</p>
+                  </div>
+                </div>
+              </div>
+              <div className="w-full mt-10 lg:mt-0 lg:pl-4 lg:w-[60%]">
+                <form ref={form} onSubmit={handleSubmit}>
+                  <div className="flex">
+                    <div className="w-[50%]">
+                      <input
+                        onChange={handleChange}
+                        value={values.user_name}
+                        type="text"
+                        name="user_name"
+                        className={`${
+                          errors.user_name && touched.user_name
+                            ? "!border-red-500"
+                            : ""
+                        } w-full shadow-lg shadow-black focus:shadow-black outline-none focus:shadow-md duration-300 border-4 border-[#333B43] bg-[#45505B] p-2 rounded-full text-white`}
+                        placeholder="Enter your name"
+                      />
+                      {errors.user_name && touched.user_name ? (
+                        <div className=" p-1 rounded-full flex items-center bg-red-100">
+                          <RxCrossCircled
+                            className="text-red-400 mr-2"
+                            size={20}
+                          />
+                          <p className="text-black font-bold">
+                            {`${errors.user_name}`}
+                          </p>
+                        </div>
+                      ) : null}
+                    </div>
+                    <div className="w-[50%] ml-4">
+                      <input
+                        onChange={handleChange}
+                        value={values.user_email}
+                        type="email"
+                        name="user_email"
+                        className={`${
+                          errors.user_email && touched.user_email
+                            ? "!border-red-500"
+                            : ""
+                        } w-full shadow-lg shadow-black focus:shadow-black outline-none focus:shadow-md duration-300 border-4 border-[#333B43] bg-[#45505B] p-2 rounded-full text-white`}
+                        placeholder="Enter your email"
+                      />
+                      {errors.user_email && touched.user_email ? (
+                        <div className=" p-1 rounded-full flex items-center bg-red-100">
+                          <RxCrossCircled
+                            className="text-red-400 mr-2"
+                            size={20}
+                          />
+                          <p className="text-black font-bold">
+                            {`${errors.user_email}`}
+                          </p>
+                        </div>
+                      ) : null}
+                    </div>
+                  </div>
+                  <div className="">
+                    <input
+                      onChange={handleChange}
+                      value={values.user_subject}
+                      name="user_subject"
+                      type="text"
+                      className={`${
+                        errors.user_subject && touched.user_subject
+                          ? "!border-red-500"
+                          : ""
+                      } shadow-lg shadow-black focus:shadow-black focus:shadow-md outline-none duration-300 border-4 border-[#333B43] bg-[#45505B] w-full p-2 mt-4 rounded-full text-white`}
+                      placeholder="Your subject"
+                    />
+                    {errors.user_subject && touched.user_subject ? (
+                      <div className=" p-1 rounded-full flex items-center bg-red-100">
+                        <RxCrossCircled
+                          className="text-red-400 mr-2"
+                          size={20}
+                        />
+                        <p className="text-black font-bold">
+                          {`${errors.user_subject}`}
+                        </p>
+                      </div>
+                    ) : null}
+                  </div>
+                  <div className="">
+                    <textarea
+                      onChange={handleChange}
+                      value={values.user_message}
+                      name="user_message"
+                      rows={7}
+                      className={`${
+                        errors.user_message && touched.user_message
+                          ? "!border-red-500"
+                          : ""
+                      } shadow-lg shadow-black focus:shadow-black focus:shadow-md outline-none duration-300 first-letter border-4 border-[#333B43] bg-[#45505B] w-full p-2 mt-4 rounded-2xl text-white`}
+                      placeholder="Write your message here..."
+                    />
+                    {errors.user_message && touched.user_message ? (
+                      <div className=" p-1 rounded-full flex items-center bg-red-100">
+                        <RxCrossCircled
+                          className="text-red-400 mr-2"
+                          size={20}
+                        />
+                        <p className="text-black font-bold">
+                          {`${errors.user_message}`}
+                        </p>
+                      </div>
+                    ) : null}
+                  </div>
+                  <button
+                    type="submit"
+                    className="bg-[#ffb400] border-4 border-[#9A7318] text-white shadow-xl shadow-black hover:shadow-md hover:shadow-black mt-3 py-1 px-3 rounded-full duration-300 hover:scale-95"
+                    disabled={loading}
+                  >
+                    {loading ? "sending... " : "Send message"}
+                  </button>
+                </form>
+              </div>
             </div>
-            <p className="pt-2 text-left font-body font-bold text-primary lg:text-lg">
-              +923171370439
-            </p>
-          </div>
-          <div className="w-full border-l-2 border-t-0 border-r-2 border-b-2 border-grey-60 px-6 py-6 sm:py-8 lg:w-1/3 lg:border-l-0 lg:border-t-2">
-            <div className="flex items-center">
-              <AiOutlineMail className="text-2xl text-grey-40" />
-              <p className="pl-2 font-body font-bold uppercase text-grey-40 lg:text-lg">
-                My Email
-              </p>
-            </div>
-            <p className="pt-2 text-left font-body font-bold text-primary lg:text-lg">
-              ahsan3916902@gmail.com
-            </p>
-          </div>
-          <div className="w-full border-l-2 border-t-0 border-r-2 border-b-2 border-grey-60 px-6 py-6 sm:py-8 lg:w-1/3 lg:border-l-0 lg:border-t-2">
-            <div className="flex items-center">
-              <BiCurrentLocation className="text-2xl " />
-              <p className="pl-2 font-body font-bold uppercase text-grey-40 lg:text-lg">
-                My Address
-              </p>
-            </div>
-            <p className="pt-2 text-left font-body font-bold text-primary lg:text-lg">
-              Hyderabad, Sindh, Pakistan
-            </p>
           </div>
         </div>
       </div>
-    </section>
+    </>
   );
-};
-
-export default Contact;
+}
