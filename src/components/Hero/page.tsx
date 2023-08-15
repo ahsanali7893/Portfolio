b8ce79bbd49e8db742b3f3af3811780003e60cd2
ahsanali7893 <ahsan3916902@gmail.com>
2023-08-15 11:57:31 +0500

    initial changes

--- src/components/Hero/page.tsx
+++ src/components/Hero/page.tsx
@@ -9,7 +9,7 @@ const Hero = (props: Props) => {
   return (
     <div>
       <div
-        className="relative bg-cover bg-center bg-no-repeat py-8 max-h-screen items-center justify-center "
+        className="bg-gradient-to-r from-teal-500 to-cyan-600 py-16 min-h-screen flex items-center justify-center "
         style={{ backgroundColor: "rgba(0, 128, 128, 0.6)" }}
       >
         <div className="absolute inset-0 z-20 bg-gradient-to-r from-hero-gradient-from to-hero-gradient-to bg-cover bg-center bg-no-repeat"></div>
@@ -24,15 +24,18 @@ const Hero = (props: Props) => {
                 alt=""
               />
             </div>
-            <div className="pt-8 sm:pt-10 lg:pl-8 lg:pt-0">
-              <h1 className="text-center font-header text-4xl text-white sm:text-left sm:text-5xl md:text-6xl px-3">
+            <div className="pt-3 sm:pt-5 lg:pl-8">
+              <h1 className="text-center font-header pb-3 text-4xl text-white sm:text-left sm:text-5xl md:text-6xl px-3">
                 Hello
                 <span className="text-pink-600 text-3xl sm:text-5xl pl-2">
                   I am Ahsan Ali
                 </span>
               </h1>
+              <p className="text-lg px-3 sm:text-xl text-gray-300">
+                Frontend Developer | UI/UX Enthusiast
+              </p>
 
-              <div className="flex flex-col justify-center pt-3 sm:flex-row sm:pt-5 lg:justify-start">
+              <div className="flex flex-col justify-center sm:flex-row px-3 lg:justify-start">
                 <div className="flex items-center justify-center pl-0 sm:justify-start md:pl-1">
                   <p className="font-body text-lg uppercase text-white">
                     Let&apos;s Connect
@@ -67,12 +70,13 @@ const Hero = (props: Props) => {
                     <BsFacebook />
                   </Link>
                 </div>
-                  <Link
-                    href="/resume.pdf" target="_blank"
-                    className="my-6 px-28 py-3 sm:px-6 sm:py-3 sm:ml-4 rounded-lg bg-gradient-to-r from-purple-500 to-pink-500 text-white font-semibold shadow-md hover:shadow-lg transform hover:scale-105 transition duration-300 ease-in-out"
-                  >
-                    Download CV
-                  </Link>
+                <Link
+                  href="/resume.pdf"
+                  target="_blank"
+                  className="my-6 px-28 py-3 sm:px-6 sm:py-3 sm:ml-4 rounded-lg bg-gradient-to-r from-purple-500 to-pink-500 text-white font-semibold shadow-md hover:shadow-lg transform hover:scale-105 transition duration-300 ease-in-out"
+                >
+                  Download CV
+                </Link>
               </div>
             </div>
           </div>
