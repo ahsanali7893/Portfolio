33bedc6ea863e489e3e14c0db0cbd0e4fe23d6ad
ahsanali7893 <ahsan3916902@gmail.com>
2023-08-15 13:25:10 +0500

    initial changes

--- src/components/Skill/page.tsx
+++ src/components/Skill/page.tsx
@@ -1,131 +1,49 @@
 import React from "react";
 
-type Props = {};
+const Skill = () => {
+  const skills = [
+    { name: "HTML", imgSrc: "./html.png", bgColor: "orange-500" },
+    { name: "CSS", imgSrc: "./css.png", bgColor: "blue-600" },
+    { name: "Tailwind", imgSrc: "./tailwind.png", bgColor: "blue-700" },
+    { name: "JavaScript", imgSrc: "./js.png", bgColor: "yellow-500" },
+    { name: "TypeScript", imgSrc: "./ts.png", bgColor: "blue-400" },
+    { name: "React JS", imgSrc: "./react.png", bgColor: "sky-500" },
+    { name: "Bootstrap", imgSrc: "./bs.png", bgColor: "sky-500" },
+    { name: "Mantine", imgSrc: "./Mantine.png", bgColor: "sky-500" },
+    { name: "GitHub", imgSrc: "./github.png", bgColor: "sky-500" },
+    { name: "Git", imgSrc: "./git.png", bgColor: "red-400" },
+    { name: "NEXT JS", imgSrc: "./next.png", bgColor: "gray-400" },
+  ];
 
-const Skill = (props: Props) => {
   return (
-    <div>
-      <section id="skill">
-        <div className="container py-16 md:py-20 max-w-6xl mx-auto">
-          <h2 className="text-center font-header text-4xl font-semibold uppercase text-[#66b3b3] text-primary sm:text-5xl lg:text-6xl">
-            Skills
-          </h2>
-          <div className="grid grid-cols-1 gap-6 pt-10 sm:grid-cols-2 md:gap-10 md:pt-12 lg:grid-cols-4">
-            <div className="group rounded-full px-8 py-12">
-              <div className="mx-auto h-24 w-24 text-center xl:h-28 xl:w-28">
-                <div className="transform transition-all hover:scale-110 duration-500">
-                  <img
-                    src="./html.png"
-                    alt="html"
-                    className="cursor-pointer "
-                  />
-                  <p className="font-bold hover:text-orange-500 duration-500 cursor-pointer">
-                    HTML
-                  </p>
-                </div>
+    <section id="skill" className="py-16 md:py-20 bg-gray-50">
+      <div className="container max-w-5xl mx-auto">
+        <h2 className="text-center font-header text-4xl font-semibold uppercase text-[#66b3b3] text-primary sm:text-5xl lg:text-6xl">
+          Skills
+        </h2>
+        <div className="grid grid-cols-1 gap-6 mt-10 sm:grid-cols-2 md:gap-10 md:mt-12 lg:grid-cols-4">
+          {skills.map((skill, index) => (
+            <div
+              key={index}
+              className={`group rounded-full overflow-hidden hover:shadow-lg transition-transform transform hover:scale-105 duration-300 bg-${skill.bgColor} px-8 py-12 text-center`}
+            >
+              <div className="mx-auto h-24 w-24 xl:h-28 xl:w-28">
+                <img
+                  src={skill.imgSrc}
+                  alt={skill.name}
+                  className={`cursor-pointer hover:filter hover:brightness-125 transition duration-300`}
+                />
+                <p
+                  className={`font-bold mt-2 text-white hover:text-white`}
+                >
+                  {skill.name}
+                </p>
               </div>
             </div>
-            <div className="group rounded  px-8 py-12">
-              <div className="mx-auto h-24 w-24 text-center xl:h-28 xl:w-28">
-                <div className="transform transition-all hover:scale-110 duration-500">
-                  <img src="./css.png" alt="html" className="cursor-pointer " />
-                  <p className="font-bold hover:text-blue-600 duration-500 cursor-pointer">
-                    CSS
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="group rounded px-8 py-12">
-              <div className="mx-auto h-24 w-24 text-center xl:h-28 xl:w-28">
-                <div className="transform transition-all hover:scale-110 duration-500">
-                  <img
-                    src="./tailwind.png"
-                    alt="html"
-                    className="cursor-pointer "
-                  />
-                  <p className="font-bold hover:text-blue-700 duration-500 cursor-pointer">
-                    Tailwind
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="group rounded px-8 py-12">
-              <div className="mx-auto h-24 w-24 text-center xl:h-28 xl:w-28">
-                <div className="transform transition-all hover:scale-110 duration-500">
-                  <img src="./js.png" alt="html" className="cursor-pointer " />
-                  <p className="font-bold hover:text-yellow-500 duration-500 cursor-pointer">
-                    JavaScript
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="group rounded px-8 py-12">
-              <div className="mx-auto h-24 w-24 text-center xl:h-28 xl:w-28">
-              <div className="transform transition-all hover:scale-110 duration-500">
-                  <img
-                    src="./react.png"
-                    alt="html"
-                    className="cursor-pointer "
-                  />
-                  <p className="font-bold hover:text-sky-500 duration-500 cursor-pointer">
-                    React JS
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="group rounded px-8 py-12">
-              <div className="mx-auto h-24 w-24 text-center xl:h-28 xl:w-28">
-              <div className="transform transition-all hover:scale-110 duration-500">
-                  <img
-                    src="./bs.png"
-                    alt="html"
-                    className="cursor-pointer "
-                  />
-                  <p className="font-bold hover:text-sky-500 duration-500 cursor-pointer">
-                    Bootstrap
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="group rounded px-8 py-12">
-              <div className="mx-auto h-24 w-24 text-center xl:h-28 xl:w-28">
-              <div className="transform transition-all hover:scale-110 duration-500">
-                  <img
-                    src="./Mantine.png"
-                    alt="html"
-                    className="cursor-pointer "
-                  />
-                  <p className="font-bold hover:text-sky-500 duration-500 cursor-pointer">
-                    Mantine
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="group rounded px-8 py-12">
-              <div className="mx-auto h-24 w-24 text-center xl:h-28 xl:w-28">
-              <div className="transform transition-all hover:scale-110 duration-500">
-                  <img
-                    src="./github.png"
-                    alt="html"
-                    className="cursor-pointer "
-                  />
-                  <p className="font-bold hover:text-sky-500 duration-500 cursor-pointer">
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div className="group rounded px-5 py-12 justify-center">
-              <div className="mx-auto h-24 w-24 text-center xl:h-28 xl:w-28">
-                <div className="transform transition-all hover:scale-110 duration-500">
-                  <img src="./next.png" alt="html" className="cursor-pointer" />
-                  <p className="font-bold cursor-pointer">NEXT JS</p>
-                </div>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
   );
 };
 
